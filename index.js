eb4ddfa6b47f6718cfd587dc251cd7f7d21cb8d4
wooden1 <lynnwoodray7@gmail.com>
2020-07-24 22:49:55 -0400

    added checkboxes for course selection, minor css tweaks

--- index.js
+++ index.js
@@ -20,13 +20,80 @@ function fetchData(url) {
     .then(checkStatus)
     .then(res => res.json())
 }
+// function updateStudentInfo(student, name){
+
+// }
+function addCoursesandStudents(student, course) {
+  const selectedCourse = document.getElementById('course-selection').selectedOptions
+  const targetStudent = this.students.find(s => s.id === student.id)
+  targetStudent.courses.push(course)
+  const targetCourse = this.courses.find(c => c.id === course.id)
+  targetCourse.students.push(student)
+}
+// function addCourse(student, course) {
+//   if (!student.courses) {
+//     student.courses = [course]
+//   } else {
+//     student.courses.push(course)
+//   }
+// }
+
+
+// returns an alert if enrolled in more than 4 classes
+function checkCoursesAllowed(student, course) {
+  if (student.courses && student.courses.length >= 4) {
+    const courseOption = document.getElementById('course-options')
+
+    courseOption.innerHTML = `<div class="alert alert-warning alert-dismissible fade show" role="alert">
+  <strong>Sorry!</strong>, you are already assigned to the maximum number of classes.
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>`
+    throw Error('Only allowed to be in 4 classes')
+  }
+  addCoursesandStudents(student, course)
+}
+
 // Adds course to student card
-function appendCourses(data) {
-  const courseList = document.querySelector('.course-list')
-  const select = document.createElement('select')
-  select.innerHTML = `${data[1].map(el =>
-    `<option value="${el.name}">${el.name}</option>`)}`
-  courseList.appendChild(select)
+async function appendCard(data) {
+  const students = await data[0]
+  const courses = await data[1]
+  const studentSection = document.getElementById('student-section')
+  const div = document.createElement('div')
+
+  div.className = 'row'
+  div.innerHTML = `${students.map(el =>
+    `<div class="card col-md-4" style="width: 20rem;">
+    <div class-"card-header">
+    <h5>${el.name} ${el.last_name}</h5>${
+  el.status
+    ? '<span class="status"></span>'
+    : '<span class="status" style="visibility: hidden;"></span>'
+}
+    </div>
+  <div class="card-body">
+    <div id="course-options" class="modal" role="dialog" tabindex="-1" aria-hidden="true">
+    <div class="modal-header">
+      <label for="courses" class="modal-title">Join a course</label>
+      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+    <fieldset>
+    <legend>Choose up to 4 courses</legend>
+    ${courses.map(element => `<div>
+   <input type="checkbox" id="${element.name}" name="courses" value="${element.name}">
+    <label for="${element.name}">${element.name}</label>
+    </div>`)}
+    </fieldset>
+    </div>
+    <button data-toggle="modal" data-target="#course-options" class="btn btn-outline-info card-link">Add Course</button>
+    <button class="btn btn-outline-info card-link">Edit Info</button>
+  </div>
+</div>`)}`
+
+  studentSection.appendChild(div)
 }
 function createCourseCard(data) {
   const courseSection = document.getElementById('course-section')
@@ -40,36 +107,19 @@ function createCourseCard(data) {
         <button class="btn btn-outline-info card-link" data-toggle="collapse" data-target="#add-student" aria-expanded="false" aria-controls="add-student">Add student</button>
       </div>
     </div>`)}`
+
   console.log(data)
   courseSection.appendChild(div)
-}
-// Creates student cards of users  in database
-function createStudentCard(data) {
-  const studentSection = document.getElementById('student-section')
-  const div = document.createElement('div')
-
-  div.className = 'row'
-  div.innerHTML = `${data[0].map(el =>
-    `<div class="card col-md-4" style="width: 20rem;">
-  <div class="card-body">
-    <h5 class="card-title">${el.name} ${el.last_name}</h5>
-   <div id="course-options" class="card-text course-list "></div>
-    <button data-toggle="collapse" data-target="#course-options" aria-expanded="false"
-          aria-controls="course-options" class="btn btn-outline-info card-link">Add Course</button>
-    <button class="btn btn-outline-info card-link">Edit Info</button>
-  </div>
-</div>`)}`
-
-  studentSection.appendChild(div)
 }
 
 function getStudents() {
   Promise.all([fetchData(studentURl), fetchData(coursesURL)])
     .then((values) => {
       // console.log(values)
-      createStudentCard(values)
+      // createStudentCard(values)
+      appendCard(values)
     })
-    .catch(err => console.log(err))
+    .catch((err) => { throw Error(err) })
 }
 function getCourses() {
   Promise.all([fetchData(studentURl), fetchData(coursesURL)])
@@ -77,7 +127,7 @@ function getCourses() {
       // console.log(values)
       createCourseCard(values)
     })
-    .catch(err => console.log(err))
+    .catch((err) => { throw Error(err) })
 }
 
 studentBtn.addEventListener('click', getStudents)
